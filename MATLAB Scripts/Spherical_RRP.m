3773fb9b277f5ebdb3401dd26cd5eb479ce9b470
Stephen Gabriel S. Alojado <21-07082@g.batstate-u.edu.ph>
2024-02-26 09:16:20 +0800

    Update Spherical_RRP.m

--- MATLAB Scripts/Spherical_RRP.m
+++ MATLAB Scripts/Spherical_RRP.m
@@ -4,7 +4,7 @@ syms a1 a2 a3 a4 t1 t2 d3
 
 %% Link Lengths
 
-a1 = 7;
+a1 = 5;
 a2 = 2;
 a3 = 2;
 
@@ -19,7 +19,7 @@ d3 = 3;
 % if revolute joint: theta = 0, offset = 0, after offset put the value of theta
 
 H0_1 = Link([0,a1,0,pi/2,0,t1]); 
-H0_1.qlim = pi/180*[-90 90];
+H0_1.qlim = pi/180*[-180 180];
 
 H1_2 = Link([0,0,0,pi/2,0,t2]);
 H1_2.qlim = pi/180*[0 180];
@@ -30,5 +30,5 @@ H2_3.qlim = [0 d3];
 %% Build Mechanical Manipulator
 
 Spherical = SerialLink([H0_1 H1_2 H2_3], 'name', 'Spherical')
-Spherical.plot([0 0 0], 'workspace', [-10 10 -10 10 0 20])
+Spherical.plot([0 0 0], 'workspace', [-8 8 -8 8 -1 13])
 Spherical.teach
