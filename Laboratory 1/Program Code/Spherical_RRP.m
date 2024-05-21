8008d48d368f64e1241694113b51235bc829e920
Stephen Gabriel S. Alojado <21-07082@g.batstate-u.edu.ph>
2024-05-21 12:37:19 +0800

    Update Spherical_RRP.m

--- Laboratory 1/Program Code/Spherical_RRP.m
+++ Laboratory 1/Program Code/Spherical_RRP.m
@@ -6,13 +6,13 @@ syms a1 a2 a3 a4 t1 t2 d3
 
 a1 = 50;
 a2 = 20;
-a3 = 20;
+a3 = 15;
 
 %% Joint Variables
 
-t1 = 0;
+t1 = pi/180*(0);
 t2 = pi/180*(90);
-d3 = 30;
+d3 = 25;
 
 %% D-H Parameters: [theta, d, r, alpha, offset]
 % if prismatic joint: theta = theta, d = 0, offset = 1, after offset put the value of d
@@ -32,9 +32,4 @@ H2_3.qlim = [0 d3];
 Spherical = SerialLink([H0_1 H1_2 H2_3], 'name', 'Spherical')
 Spherical.plot([0 0 0], 'workspace', [-80 80 -80 80 -10 130])
 Spherical.teach
-
-
-q_j1 = [45*pi/180 90*pi/180 5];
-J1 = jacob0(Spherical, q_j1)
-
 
